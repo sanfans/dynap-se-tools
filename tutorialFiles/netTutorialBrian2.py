5a67aa24384bcf74ed68942432bc6df1df53638b
Roberto Cattaneo <cattaroby93@hotmail.it>
2018-04-08 15:08:23 +0200

    Fixed documentation and net tutorial

--- tutorialFiles/netTutorialBrian2.py
+++ tutorialFiles/netTutorialBrian2.py
@@ -8,58 +8,81 @@
 import DYNAPSETools.dynapseNetGenerator as DNG
 import numpy as np
 import matplotlib.pyplot as plt
+from brian2 import start_scope, Network, NeuronGroup, Synapses,\
+SpikeGeneratorGroup, SpikeMonitor, metre, second, volt, mV, ms, us,\
+defaultclock, prefs
 
 np.random.seed(0) # Set seed for the random number generation
 plt.close("all") # Close all the plots that has been created before
+prefs.codegen.target = "numpy"
 
+############ BRIAN2 NETWORK
+# Populations definition
+brianInputLayer = NeuronGroup(1, model = "", name='brianInputLayer')
+brianMiddleLayer = NeuronGroup(10, model = "", name='brianMiddleLayer')
+brianOutputLayer = NeuronGroup(2, model = "", name='brianOutputLayer')
+
+middleLayerInhNeurons = [5, 6, 7, 8, 9]
+
+# Connections definition
+brianInputLayerTobrianMiddleLayer = Synapses(brianInputLayer,
+                                             brianMiddleLayer,
+                                             model = "w : 1",
+                                             name = "brianInputLayerTobrianMiddleLayer")
+brianInputLayerTobrianMiddleLayer.connect(True)
+brianInputLayerTobrianMiddleLayer.w = 1
+
+brianMiddleLayerTobrianOutputLayer = Synapses(brianMiddleLayer,
+                                              brianOutputLayer,
+                                              model = "w : 1",
+                                              name = "brianInputLayerTobrianMiddleLayer")
+brianMiddleLayerTobrianOutputLayer.connect(condition = "i < 5 and j == 0")
+brianMiddleLayerTobrianOutputLayer.connect(condition = "i >= 5 and j == 1")
+brianMiddleLayerTobrianOutputLayer.w = 1
+
+brianMiddleLayerTobrianOutputLayer2 = Synapses(brianMiddleLayer,
+                                               brianOutputLayer, model = "w : 1",
+                                               name = "brianMiddleLayerTobrianOutputLayer2")
+brianMiddleLayerTobrianOutputLayer2.connect(condition = "i < 5 and j == 1")
+brianMiddleLayerTobrianOutputLayer2.w = 1
+
+############ CONVERSION TO DYNAPSE
+# Place populations
 U, C, N = 4, 0, 1
-size = 1
-InputLayer = DNG.DevicePopulation(name = "InputLayer",
-                                   chip_id = U, core_id = C, start_neuron = N,
-                                   size = size, neuronsType = "fExc")
+InputLayer = DNG.DevicePopulation(neuronsObj = brianInputLayer,
+                                  chip_id = U, core_id = C,
+                                  neurons_id = N,
+                                  neuronsType = "fExc")
 
 U, C, start_neuron = 0, 1, 0
 size = 10
-MiddleLayer = DNG.DevicePopulation(name = "MiddleLayer",
-                                   chip_id = U, core_id = C, start_neuron = start_neuron,
-                                   size = size)
+MiddleLayer = DNG.DevicePopulation(neuronsObj = brianMiddleLayer,
+                                   chip_id = U, core_id = C,
+                                   start_neuron = start_neuron)
 
-neuronsIndexes = [5, 6, 7, 8, 9] # Choose neurons
-MiddleLayer[neuronsIndexes]["neuronsType"] = "fInh" # Set to fast inhibitory
+# Set to fast inhibitory
+MiddleLayer[middleLayerInhNeurons]["neuronsType"] = "fInh" 
 
-########## For random distribution of neurons type
-#populationIndexes = np.arange(0, size) # From 0 to size - 1
-#neuronsIndexes = np.random.permutation(populationIndexes) # Get a shuffled list
-#neuronsIndexes = neuronsIndexes[:int(size / 2)] # Take half of them
-#MiddleLayer[neuronsIndexes]["neuronsType"] = "fInh" # Set to fast inhibitory
-
+# Convert connections
 U, C = 0, 1
 N = [16, 32]
-OutputLayer = DNG.DevicePopulation(name = "OutputLayer",
-                                   chip_id = U, core_id = C, neurons_id = N,
+OutputLayer = DNG.DevicePopulation(neuronsObj = brianOutputLayer,
+                                   chip_id = U, core_id = C,
+                                   neurons_id = N,
                                    neuronsType = "fExc")
 
-size = 10 # 10 connections
-i = np.zeros(size) # i = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] -> the only input neuron
-j = np.arange(size) # j = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] -> all MiddleLayer neurons
-w = np.ones(size) # all weights equal to 1
 InputLayerToMiddleLayer = DNG.DeviceConnections(sourcePop = InputLayer,
                                                 targetPop = MiddleLayer,
-                                                i = i, j = j, w = w)
+                                                synapsesObj = brianInputLayerTobrianMiddleLayer)
 
-i =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] # All Middle neurons
-j = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1] # Half to 0 and half to 1
-w = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] # all weights equal to 1
 MiddleLayerToOutputLayer = DNG.DeviceConnections(sourcePop = MiddleLayer,
                                                  targetPop = OutputLayer,
-                                                 i = i, j = j, w = w)
-i =  [0, 1, 2, 3, 4] # All Middle neurons
-j = [1, 1, 1, 1, 1] # Half to 0 and half to 1
-w = [1, 1, 1, 1, 1] # all weights equal to 1
+                                                 synapsesObj = brianMiddleLayerTobrianOutputLayer)
+
 MiddleLayerToOutputLayer2 = DNG.DeviceConnections(sourcePop = MiddleLayer,
-                                                 targetPop = OutputLayer,
-                                                 i = i, j = j, w = w,
-                                                 connTypes = "fInh")
+                                                  targetPop = OutputLayer,
+                                                  synapsesObj = brianMiddleLayerTobrianOutputLayer2,
+                                                  connTypes = "fInh")
 
 DNG.write_connections(InputLayerToMiddleLayer, MiddleLayerToOutputLayer, MiddleLayerToOutputLayer2,
-                      fileName = "NetGenTutorial.txt")
+                      fileName = "NetGenTutorialBrian2.txt")
